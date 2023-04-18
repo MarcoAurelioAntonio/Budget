b39880a7cd54fad19d4e8f44485a9d5f3cc4a64d
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-19 04:40:12 +0600

    Add validations test cases for inventory model.

---
+++ spec/models/inventory_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Inventory, type: :model do
+  let(:user) { User.create(name: 'Shahadat Hossain', email: 'shahadat@example.com', password: '12345678') }
+
+  describe 'validations' do
+    it 'requires a inventory name' do
+      inventory = Inventory.create(name: '', user_id: user.id)
+      expect(inventory).to_not be_valid
+    end
+
+    it 'requires a user' do
+      inventory = Inventory.create(name: 'Inventory 1')
+      expect(inventory).to_not be_valid
+    end
+
+    it 'is valid with all required attributes' do
+      inventory = Inventory.create(name: 'Inventory 1', user_id: user.id)
+      expect(inventory).to be_valid
+    end
+  end
+end
