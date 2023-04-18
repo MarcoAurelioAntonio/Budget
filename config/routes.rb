97f13d04868e24495ab5758a5b083e561e60e43e
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-18 16:12:52 -0300

    Changes in food views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   root "users#index"
   resources :users
+  resources :foods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_173553) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "role"
+    t.string "role", default: "user", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
