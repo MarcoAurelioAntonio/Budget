4e8f331067b18c4adeeaa3d185ca797ab6512493
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-18 15:06:34 -0300

    Fix offneses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,15 +69,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def user_params
-    params.require(:user).permit(:name, :role)
-  end
   # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:name, :role)
