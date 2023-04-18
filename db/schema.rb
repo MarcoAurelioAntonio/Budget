a5a70ec9cb117854a85d116570479d8729d66468
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-18 07:24:43 -0500

    Add default role to users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_221853) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_092706) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "role"
+    t.string "role", default: "user", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
