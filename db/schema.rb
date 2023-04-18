524b445932db7c4836a29b7a56747836b6d9a972
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-18 14:30:25 -0300

    Add code to change price column and run rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_164745) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_172726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
-    t.decimal "price"
+    t.decimal "price", precision: 10, scale: 2
     t.integer "quantity"
     t.integer "user_id"
     t.datetime "created_at", null: false
