9bb2def4ad09489c3a8471ca91de3be7e4030824
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-18 11:44:08 -0500

    Ensure one-to-many rel between user and recipes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+  has_many :recipes, foreign_key: 'user_id', dependent: :destroy
   validates :name, presence: true, length: { maximum: 250 }
 
   def admin?
