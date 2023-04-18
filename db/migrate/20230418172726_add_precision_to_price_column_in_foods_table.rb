f2b9ebdba4de4e914b12558f9e4da709e5927b46
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-18 14:28:58 -0300

    rails generate migration AddPrecisionToPriceColumnInFoodsTableand run rails db:migrate

---
+++ db/migrate/20230418172726_add_precision_to_price_column_in_foods_table.rb
@@ -0,0 +1,4 @@
+class AddPrecisionToPriceColumnInFoodsTable < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_221853) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_164745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.decimal "price"
+    t.integer "quantity"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
