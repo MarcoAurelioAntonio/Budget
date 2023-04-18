64c5c8005e56b5a1481b9bedded48ce734b4a83a
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-18 11:42:38 -0500

    Scaffolding recipes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root "users#index"
+  root "recipes#index"
   resources :users
+  resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
