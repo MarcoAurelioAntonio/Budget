83fa254dff33003c0dcb5bfa5e90b757e2bc7ae5
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-18 14:39:40 -0300

    rails generate migration AddForeignKeyAndIndexToFoods and fill with content to point to users table

--- db/migrate/20230418164745_create_foods.rb
+++ db/migrate/20230418164745_create_foods.rb
@@ -10,4 +10,5 @@ class CreateFoods < ActiveRecord::Migration[7.0]
       t.timestamps
     end
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_172726) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_18_173553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_172726) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_172726) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
 end
