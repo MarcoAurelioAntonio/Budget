fb10a1ce3fa580962c500249bc5cf5ae8e3a3b02
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-19 16:40:02 -0300

    Create new migration to delete columns quantiity and user_id in foods table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_212700) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_192802) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,11 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_212700) do
     t.string "name"
     t.string "measurement_unit"
     t.decimal "price", precision: 10, scale: 2
-    t.integer "quantity"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "inventories", force: :cascade do |t|
@@ -52,6 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_212700) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "foods", "users"
   add_foreign_key "inventories", "users"
 end
