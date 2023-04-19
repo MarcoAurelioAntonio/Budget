3bf8c1ec9c8d5960d32ee09d20975ba2eb5d1c0c
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-19 23:00:20 +0600

    Setup inventories show controller to show current user specific inventory.

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -5,4 +5,23 @@ class InventoriesController < ApplicationController
     @user = current_user
     @inventories = @user.inventory
   end
+
+  def show
+    @inventory = Inventory.find_by(id: params[:id])
+
+    if @inventory
+      if @inventory.user != current_user
+        redirect_to inventories_path, notice: 'You are not authorized to view this inventory.'
+      end
+      @user = @inventory.user
+    else
+      redirect_to inventories_path, notice: "Inventory not found with id = #{params[:id]}"
+    end
+  end
+
+  def new; end
+
+  def create; end
+
+  def destroy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users
   resources :foods
-  resources :inventories, only: [:index]
+  resources :inventories, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
