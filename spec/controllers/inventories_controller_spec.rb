68ccda89a0c3e8b047ba693819412c951e3ddee9
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-20 00:33:34 +0600

    Add test cases for show controller when user is not logged in

--- spec/controllers/inventories_controller_spec.rb
+++ spec/controllers/inventories_controller_spec.rb
@@ -148,5 +148,18 @@ RSpec.describe InventoriesController, type: :controller do
         expect(response).to render_template(:new)
       end
     end
+
+    context 'when user is not logged in' do
+      it 'redirects to the login page' do
+        get :new
+        expect(response).to redirect_to new_user_session_path
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it 'returns http redirect' do
+        get :new
+        expect(response).to have_http_status(:redirect)
+      end
+    end
   end
 end
