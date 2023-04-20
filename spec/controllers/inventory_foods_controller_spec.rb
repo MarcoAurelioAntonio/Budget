e0509c6fbeebe36603996cae9d1e2cb097f6947b
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-21 03:20:34 +0600

    Add test cases for new InventoryFoods controller when user is logged in

---
+++ spec/controllers/inventory_foods_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe InventoryFoodsController, type: :controller do
+  let(:user) { User.create(name: 'Shahadat Hossain', email: 'test@example.com', password: '12345678') }
+  let(:inventory) { Inventory.create(name: 'Inventory 1', user:) }
+  let(:food) { Food.create(name: 'Fettuccine Alfredo', quantity: '23', price: 10.0) }
+  let(:inventory_food) { InventoryFood.create(id: 97, quantity: 5, inventory:, food:) }
+
+  describe 'GET #new' do
+    context 'when user is logged' do
+      before do
+        user.confirm
+        sign_in user
+      end
+
+      it 'returns a success response' do
+        get :new, params: { inventory_id: inventory.id }
+        expect(response).to be_successful
+      end
+
+      it 'assigns a new inventory food' do
+        get :new, params: { inventory_id: inventory.id }
+
+        expect(assigns(:inventory_food)).to be_a_new(InventoryFood)
+      end
+
+      it 'assigns to current inventory' do
+        get :new, params: { inventory_id: inventory.id }
+
+        expect(assigns(:inventory_food).inventory).to eq(inventory)
+      end
+
+      it 'renders the new template' do
+        get :new, params: { inventory_id: inventory.id }
+
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+end
