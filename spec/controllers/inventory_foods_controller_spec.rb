5a3bf721405abdff83a4ed1572d7eec1c18d42fd
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-21 09:14:20 +0600

    Add test cases for create controller when user is logged in and the food already exists

--- spec/controllers/inventory_foods_controller_spec.rb
+++ spec/controllers/inventory_foods_controller_spec.rb
@@ -52,4 +52,35 @@ RSpec.describe InventoryFoodsController, type: :controller do
       end
     end
   end
+
+  describe 'POST #create' do
+    let(:user) { User.create(name: 'Shahadat Hossain', email: 'test@example.com', password: '12345678') }
+    let(:other_user) { User.create(name: 'Shahadat Hossain2', email: 'test2@example.com', password: '12345678') }
+    let(:inventory) { Inventory.create(name: 'Inventory 1', user:) }
+    let(:other_inventory) { Inventory.create(name: 'Inventory 2', user: other_user) }
+    let(:food) { Food.create(name: 'Fettuccine Alfredo', quantity: '23', price: 10.0) }
+
+    context 'when user is logged in and the food already exists' do
+      before do
+        user.confirm
+        sign_in user
+
+        InventoryFood.create(inventory_id: inventory.id, food_id: food.id, quantity: 10)
+        post :create,
+             params: { inventory_id: inventory.id, inventory_food: { food_id: food.id, quantity: 5 } }
+      end
+
+      it 'updates the existing inventory_food record' do
+        expect(inventory.inventory_foods.find_by(food_id: food.id).quantity).to eq(15)
+      end
+
+      it 'redirects to the inventory page' do
+        expect(response).to redirect_to(inventory_path(inventory))
+      end
+
+      it 'returns http redirect' do
+        expect(response).to have_http_status(:redirect)
+      end
+    end
+  end
 end
