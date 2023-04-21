d7fee901f9aafd7e7534c166de5fe7e67f8a394f
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-22 05:56:50 +0600

    Add test cases for inventory food new file when user is not logged in

---
+++ spec/views/inventory_foods/new.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'invenroty_foods/new.html.erb', type: :feature do
+  let(:user) { User.create(name: 'Shahadat Hossain', email: 'test@example.com', password: '12345678') }
+  let!(:inventory) { Inventory.create(name: 'Inventory 1', user:) }
+  let!(:food) { Food.create(name: 'Fettuccine Alfredo', quantity: '23', price: 10.0) }
+  let!(:food2) { Food.create(name: 'Fettuccine Alfredo2', quantity: '23', price: 10.0) }
+
+  context 'when user is not logged in' do
+    before do
+      user.confirm
+      visit new_inventory_inventory_food_path(inventory.id)
+    end
+
+    it 'redirects to the login page' do
+      expect(page).to have_content('Log in')
+    end
+
+    it 'successfull logged in redirect to new inventory page' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+
+      expect(page).to have_content("Add Food to #{inventory.name}")
+    end
+  end
+end
