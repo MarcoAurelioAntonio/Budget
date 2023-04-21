30bd0fd7d30b36abb5a289adef643e113138ba16
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-21 11:43:37 -0500

    Adding micromodal to project.

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,5 @@ pin 'application', preload: true
 pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
+pin "micromodal", to: "https://cdn.skypack.dev/micromodal"
 pin_all_from 'app/javascript/controllers', under: 'controllers'
