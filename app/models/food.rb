921c4c57c9acb0815d64ca8edcb4f48131e257f4
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-21 16:39:30 -0500

    Fix measurement_unit typo.

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,6 +3,6 @@ class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
 
   validates :name, presence: true
-  validates :Measurement_unit, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :measurement_unit, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
