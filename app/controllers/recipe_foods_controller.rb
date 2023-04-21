ef33886d192b3f24df4bb32da70955522cdfa24c
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-21 11:45:43 -0500

    refactor actions to manage recipe_foods.

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,5 +1,8 @@
 class RecipeFoodsController < ApplicationController
-  before_action :set_recipe_food, only: %i[ show edit update destroy ]
+  # before_action :set_recipe_food, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+
+  load_and_authorize_resource
 
   # GET /recipe_foods or /recipe_foods.json
   def index
@@ -10,9 +13,11 @@ class RecipeFoodsController < ApplicationController
   def show
   end
 
-  # GET /recipe_foods/new
+  # GET /recipes/:recipe_id/recipe_foods/new
   def new
+    recipe = Recipe.find(params[:recipe_id])
     @recipe_food = RecipeFood.new
+    @recipe_food.recipe = recipe
   end
 
   # GET /recipe_foods/1/edit
@@ -25,7 +30,7 @@ class RecipeFoodsController < ApplicationController
 
     respond_to do |format|
       if @recipe_food.save
-        format.html { redirect_to recipe_food_url(@recipe_food), notice: "Recipe food was successfully created." }
+        format.html { redirect_to recipe_url(@recipe_food.recipe), notice: "Recipe food was successfully created." }
         format.json { render :show, status: :created, location: @recipe_food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +43,7 @@ class RecipeFoodsController < ApplicationController
   def update
     respond_to do |format|
       if @recipe_food.update(recipe_food_params)
-        format.html { redirect_to recipe_food_url(@recipe_food), notice: "Recipe food was successfully updated." }
+        format.html { redirect_to recipe_url(@recipe_food.recipe), notice: "Recipe food was successfully updated." }
         format.json { render :show, status: :ok, location: @recipe_food }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,10 +54,11 @@ class RecipeFoodsController < ApplicationController
 
   # DELETE /recipe_foods/1 or /recipe_foods/1.json
   def destroy
+    recipe = @recipe_food.recipe
     @recipe_food.destroy
 
     respond_to do |format|
-      format.html { redirect_to recipe_foods_url, notice: "Recipe food was successfully destroyed." }
+      format.html { redirect_to recipe_url(recipe), notice: "Recipe food was successfully destroyed." }
       format.json { head :no_content }
     end
   end
