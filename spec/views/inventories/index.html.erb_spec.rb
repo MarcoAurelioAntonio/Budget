58d739033c33cb9d6f398080d6fb11297c360f3b
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-22 03:42:49 +0600

    Add test case for inventories index view when user is not logged in

---
+++ spec/views/inventories/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'recipes/index.html.erb', type: :feature do
+  let(:user) { User.create(name: 'Shahadat Hossain', email: 'test@example.com', password: '12345678') }
+  let!(:inventory) { Inventory.create(name: 'Inventory 1', user:) }
+  let!(:inventory2) { Inventory.create(name: 'Inventory 2', user:) }
+
+  context 'when user is not logged in' do
+    before do
+      user.confirm
+      visit inventories_path
+    end
+
+    it 'redirects to the login page' do
+      expect(page).to have_content('Log in')
+    end
+
+    it 'successfull logged in redirect to inventories page' do
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+
+      click_button 'Log in'
+      puts page.body
+
+      expect(page).to have_content('All Inventories')
+    end
+  end
+end
