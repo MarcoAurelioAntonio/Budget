7c860b2d7a36ea467a7d04019c0f6f3b666ca7ed
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-21 17:56:48 -0300

    Make the controller logic works, is not done yet

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,13 +1,48 @@
 class ShoppingListController < ApplicationController
+  skip_authorization_check only: :index
   def index
-    # URL params from recipe/show
-    recipe_id = 1 #params[:recipe_id]
-    inventory_id = 2 # params[:inventory_id]
+    # URL params from recipe/show to http://localhost:3000/shopping_list?recipe_id=1&inventory_id=1
+    # @recipe = Recipe.find(params[:recipe_id])
+    # @inventory = Inventory.find(params[:inventory_id])
 
-    # Lógic to the list
-    
+    # Create the `recipe` hash with recipe data
+    recipe = {
+      "Carrot" => { quantity: 2, price: 0.5 },
+      "Tomato" => { quantity: 3, price: 0.3 },
+      "Potato" => { quantity: 1, price: 0.8 }
+    }
 
-    # View rendering
-    render :index
+    # Create the `inventory` hash with inventory data
+    inventory = {
+      "Carrot" => 4,
+      "Tomato" => 1,
+      "Potato" => 0
+    }
+
+    # Initialize hash to store shopping list data
+    shopping_list_data = {}
+
+    # Iterate over recipe foods to determine how much of each is needed
+    recipe.each do |food_name, recipe_data|
+      # Calculate quantity needed
+      quantity_needed = recipe_data[:quantity] - inventory[food_name]
+
+      # Add food to shopping list if quantity needed is positive
+      if quantity_needed > 0
+        shopping_list_data[food_name] = {
+          quantity_needed: quantity_needed,
+          price_per_unit: recipe_data[:price],
+          total_price: quantity_needed * recipe_data[:price]
+        }
+      end
+    end
+
+    @total_food_to_buy = shopping_list_data.values.sum { |data| data[:quantity_needed] }
+    @total_value_needed = shopping_list_data.values.sum { |data| data[:total_price] }
+
+
+    @shopping_list_data = shopping_list_data
+
   end
+
 end
