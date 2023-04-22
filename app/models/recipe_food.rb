843747c72fbd6a4ccbc573818c21b423b1524922
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-22 22:37:47 +0600

    Update recipe food model according to requirement

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,6 +2,8 @@ class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
 
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+
   def value
     food.price * quantity
   end
