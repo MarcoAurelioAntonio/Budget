6fa87bf31a570858ca44f427746da99d30699cc5
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-23 00:11:32 +0600

    Fix styles

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,4 +7,16 @@ class Recipe < ApplicationRecord
   validates :cook_time, presence: true
   validates :description, presence: true
   validates :user, presence: true
+
+  def total_food_items
+    recipe_foods.count
+  end
+
+  def total_price
+    total = 0
+    recipe_foods.each do |recipe_food|
+      total += recipe_food.quantity * recipe_food.food.price
+    end
+    total
+  end
 end
