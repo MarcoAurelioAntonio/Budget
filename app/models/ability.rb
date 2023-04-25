aa3519cb19db448e9ce53253f8b36e51fd612949
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-25 19:18:35 -0300

    Define authorization attributes with cancan in Ability model

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(current_user)
+    return unless current_user.present?
+
+    can: :manage, Group, user: current_user
+    can: :manage, Expense, user: current_user
+    can: :manage, GroupExpense do |group_expense|
+      group_expense.group.user == current_user
+    end
+  end
+end
