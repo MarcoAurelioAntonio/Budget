7e8908f3dfdfd0bd39471885d199cd9d5603290f
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-25 20:33:39 -0300

    Refactor ruotes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	# protect_from_forgery prepend: true
+  # protect_from_forgery prepend: true
 
   protect_from_forgery with: :exception
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
+
+  # This was creted by the command: rails g scaffold...
+
   # resources :expenses
   # resources :group_expenses
   # resources :groups
+
+  # This is the same as the below code, but with a personalized path:
+
+  #resources :groups, only: [:index, :new, :create, :edit, :update, :destroy], :path => "categories" do 
+  #  resources :expenses, only: [:index, :new, :create, :edit, :update, :destroy], :path => "transactions"
+  #end
+
   resources :users
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :categories, only: %i[index new create edit update destroy] do
+    resources :expenses, only: %i[index new create edit update destroy]
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
