7a78158d6204f85f0413aef2f383bee32fe4e9e6
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-26 18:54:04 -0300

    Tests for group model

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'Captain Rex', email: 'rex@captain.com', password: 'password')
+    @category = Group.new(author: @user, name: 'Movies', icon: 'https://someurl.com/icon.png')
+  end
+
+  context 'Testing Validations' do
+    it 'is valid with valid attributes' do
+      expect(@category).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      @category.name = nil
+      expect(@category).to_not be_valid
+    end
+
+    it 'is not valid without an icon' do
+      @category.icon = nil
+      expect(@category).to_not be_valid
+    end
+
+    it 'is not valid without user id' do
+      @category.author_id = nil
+      expect(@category).to_not be_valid
+    end
+  end
+
+  context 'Testing Associations' do
+    it 'belongs_to User' do
+      assoc = Group.reflect_on_association(:author)
+      expect(assoc.macro).to eq :belongs_to
+    end
+
+    it 'has_many group_expenses' do
+      assoc = Group.reflect_on_association(:group_expenses)
+      expect(assoc.macro).to eq :has_many
+    end
+  end
 end
