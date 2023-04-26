77f006a552df57ae698d37bb8af6b1c71c4ae3d8
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-04-26 17:54:37 -0300

    Set up validations and relationships in Expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,9 @@
 class Expense < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+	has_many :group_expenses, dependent: :destroy
+	has_many :groups, through: :group_expenses, dependent: :destroy
+
+	validates :name, presence: true, length: { maximum: 100 },
+									 uniqueness: { scope: :author, message: 'You already have an expense with this name' }
+	validates :amount, presence: true, numericality: { greater_than: 0 }
 end
