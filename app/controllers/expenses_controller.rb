8f03776866410227a34d69d50e02910201d519d1
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-05-02 18:49:10 -0300

    refactor navbar and delete and edit group functions

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,11 @@
 class ExpensesController < ApplicationController
-  before_action :set_expense, only: %i[show edit update destroy]
+  before_action :set_expense, only: %i[edit update destroy]
+  before_action :set_group, only: %i[index new edit create update destroy]
+  before_action :set_user, only: %i[index edit create update destroy]
 
   # GET /expenses or /expenses.json
   def index
-    @expenses = Expense.all
+    @expenses = @group.expenses.order(created_at: :desc)
   end
 
   # GET /expenses/1 or /expenses/1.json
@@ -58,6 +60,14 @@ class ExpensesController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @author = current_user
+  end
+
+  def set_group
+    @group = set_user.groups.find(params[:group_id])
+  end
+
   def set_expense
     @expense = Expense.find(params[:id])
   end
