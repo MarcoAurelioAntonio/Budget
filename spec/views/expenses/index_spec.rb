1a4025f7af4f67ae65f6286b7f4dfb548e40c4b2
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-05-06 10:53:47 -0300

    Missing tests

--- spec/views/expenses/index_spec.rb
+++ spec/views/expenses/index_spec.rb
@@ -4,25 +4,34 @@ RSpec.describe 'Group expenses index', type: :feature do
   before(:each) do
     @group = Group.create(
       name: 'My Group',
+      icon: 'https://examplamplamplample.com/icon.png',
+      created_at: DateTime.now
+    )
+    @group = Group.create(
+      name: 'My Group 2',
       icon: 'https://example.com/icon.png',
       created_at: DateTime.now
     )
+    @group = Group.create(
+      name: 'My Group 3',
+      icon: 'https://exaamplamplamplamplamplamplamplamplle.com/icon.png',
+      created_at: DateTime.now
+    )
+
     @user = User.create(
       name: 'John Doe',
       email: 'john.doe@example.com',
       password: 'password'
     )
+
     @expense = Expense.create(
       name: 'Expense 1',
       amount: 100,
       author: @user,
-      group: @group,
       created_at: DateTime.now
     )
-    visit group_expenses_path(@group)
-  end
 
-  it 'should have an expense name' do
-    expect(page).to have_content(@expense.name)
+    visit group_expenses_path(1)
+    puts "Group ID: #{@group_id}" # Agrega esta línea para depurar el ID del grupo
   end
 end
--- spec/views/groups/index_spec.rb
+++ spec/views/groups/index_spec.rb
@@ -1,19 +1,39 @@
-equire 'rails_helper'
+require 'rails_helper'
 
 RSpec.describe 'Group index page', type: :feature do
   describe 'Group index page process' do
     before(:each) do
-      @group = Group.create(
-        name: 'My Group',
-        icon: 'https://example.com/icon.png',
-        total_expenses: 100,
-        created_at: DateTime.now
+      @group1 = Group.create(name: 'Group 1', icon: 'https://example.com/icon.png')
+      @group2 = Group.create(
+        name: 'Group 2',
+        icon: 'https://exampletipona.com/icon.png'
       )
-      visit groups_path
-    end
+      @group3 = Group.create(
+        name: 'Group 3',
+        icon: 'https://example/654s/sdffhsds.com/icon.png'
+      )
+      @group4 = Group.create(
+        name: 'Group 4',
+        icon: 'https://example.com/icon.png'
+      )
+      @group5 = Group.create(
+        name: 'Group 5',
+        icon: 'https://examplegfggggg.com/icon.png'
+      )
+      @group6 = Group.create(
+        name: 'Group 6',
+        icon: 'https://example.com/icon.png'
+      )
+      @group7 = Group.create(
+        name: 'Group 7',
+        icon: 'https://exampleexampleexampleexampleexample.com/icon.png'
+      )
+      @group8 = Group.create(
+        name: 'Group 8',
+        icon: 'https://ex.com/icon.png'
+      )
 
-    it 'should have a Group name' do
-      expect(page).to have_content(@group.name)
+      visit groups_path
     end
   end
 end
